4db64897f185902a603a89c170485279139b13e8
basement_mustard <120338237+BasementMustard@users.noreply.github.com>
2023-08-14 18:03:35 -0600

    Update app.py

--- app.py
+++ app.py
@@ -6,4 +6,8 @@ if main_detected or corrupted_not_detected:
 else:
   print("File Hex Editor cannot run. Either main files are not found, or corrupted files are found. Re-download the files on GitHub at https://github.com/BasementMustard/File-Hex-Editor/new/main.")
 
+terms_and_conditions = open("termsandconditions.txt", "r")
 
+print(terms_and-conditions.readable())
+
+terms_and_conditions.close()
